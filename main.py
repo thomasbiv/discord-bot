6ab116d47c7e0291107d2b88eab2177407ec38d0
Thomas Bivins <bivinsthomas@gmail.com>
2024-05-03 10:59:40 -0400

    Logging

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import settings
 import discord
 from discord.ext import commands
 
+logger = settings.logging.getLogger("bot")
+
 def run():
     intents = discord.Intents.default() #<-- May change this to all() instead of default later depending on what we want
     bot = commands.Bot(command_prefix="$", intents = intents)
     
     @bot.event
     async def on_ready():
-        print(bot.user)
-        print(bot.user.id)
-        print("------------")
-        print("Initialized.")
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+        # Initial testing strings, since replaced with logging:
+        # print(bot.user)
+        # print(bot.user.id)
+        # print("------------")
+        # print("Initialized.")
     
     bot.run(settings.DISCORD_API_SECRET)
 
