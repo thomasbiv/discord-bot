e8d8582098cdb017aa89162b91580c862f77f5f0
Thomas Bivins <bivinsthomas@gmail.com>
2024-05-03 23:13:58 -0400

    Polling command functionality
    This commit establishes the polling command referenced in issue #3

--- allCommands.py
+++ allCommands.py
@@ -9,6 +9,20 @@ class allCommands(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'Pong! :ping_pong: - {round(self.bot.latency * 1000)}ms')
+
+    @commands.command()
+    async def poll(self, ctx, *, message):
+        await ctx.channel.purge(limit=1)
+        messagelist = [x.strip() for x in message.split(',')]
+        embed=discord.Embed(title = "***" + messagelist[0] + "***")
+        numbers_to_words = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+        for i in range(len(messagelist)):
+            if i > 0:
+                embed.add_field(name = "", value = numbers_to_words[i] + " " + messagelist[i], inline=False)
+        msg = await ctx.send(embed=embed)
+        for j in range(len(messagelist)):
+            if j > 0:
+                await msg.add_reaction(numbers_to_words[j])
         
 def setup(bot):
     bot.add_cog(allCommands(bot))
