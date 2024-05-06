1f04f9fc2dc8eafb92116e892334eb2902099cf7
Thomas Bivins <bivinsthomas@gmail.com>
2024-05-06 12:00:00 -0400

    cog overhaul with python update

--- cogs/allCommands.py
+++ cogs/allCommands.py
@@ -10,5 +10,5 @@ class allCommands(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'Pong! :ping_pong: - {round(self.bot.latency * 1000)}ms')
         
-def setup(bot):
-    bot.add_cog(allCommands(bot))
+async def setup(bot):
+    await bot.add_cog(allCommands(bot))
